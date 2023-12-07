661b0449f1c3bd867393bd5b00592ed194cee032
SaSharkQQ <95123180+SaSharkQQ@users.noreply.github.com>
2023-12-07 21:20:50 +0800

    Update MLMF.py

--- src/MMSA/models/multiTask/MLMF.py
+++ src/MMSA/models/multiTask/MLMF.py
@@ -189,13 +189,13 @@ class MLMF(nn.Module):
         # next we perform "tensor fusion", which is essentially appending 1s to the tensors and take Kronecker product
         add_one = torch.ones(size=[batch_size, 1], requires_grad=False).type_as(audio_h).to(text_x.device)
         _audio_h = torch.cat((add_one, audio_h), dim=1)
-        print(audio_h)
+        print(audio_h.shape)
         print(_audio_h.shape)
         _video_h = torch.cat((add_one, video_h), dim=1)
-        print(video_h)
+        print(video_h.shape)
         print(_video_h.shape)
         _text_h = torch.cat((add_one, text_h), dim=1)
-        print(text_h)
+        print(text_h.shape)
         print(_text_h.shape)
         
         fusion_audio = torch.matmul(_audio_h, self.audio_factor)
